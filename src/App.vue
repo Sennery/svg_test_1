dd8bfe4550a5104b63107fac98dc45738571619c
likhachev-ara <mac452ares@gmail.com>
2021-04-08 20:46:21 +0500

    Added font

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <div id="app">
-    <Loader/>
-  </div>
+    <div id="app">
+        <Loader/>
+        <Lines/>
+    </div>
 </template>
 
 <script>
 import Loader from './components/Loader.vue'
+import Lines from './components/Lines.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Loader
-  }
+    name: 'App',
+    components: {
+        Loader,
+        Lines
+    }
 }
 </script>
 
 <style>
-
+html {
+    font-family: "Anodina", sans-serif;
+    font-size: 1vw;
+}
+@font-face {
+    font-family: "Anodina";
+    src: url("assets/fonts/Anodina-Light.otf");
+    font-weight: normal;
+}
+@font-face {
+    font-family: "Anodina";
+    src: url("assets/fonts/Anodina-ExtraBold.otf");
+    font-weight: bold;
+}
 </style>
