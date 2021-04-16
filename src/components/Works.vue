ac4816f6a1662fe590260aa3fe453e3abdac4f9d
likhachev-ara <mac452ares@gmail.com>
2021-04-16 22:51:49 +0500

    Other component for test scroll

---
+++ src/components/Works.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <img src="../assets/123.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    z-index: -9999;
+}
+</style>
